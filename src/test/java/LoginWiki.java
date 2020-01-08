7655a70653274104bd3333673cfdb258823ea619
IlyaKoshkin444 <eliyahu.kosh44@gmail.com>
2020-01-08 15:09:44 +0200

    Add testng.xml , new build.gradle, new TestBase

--- src/test/java/LoginWiki.java
+++ src/test/java/LoginWiki.java
@@ -1,45 +1,24 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginWiki {
 
-    WebDriver driver;
-    @BeforeMethod
-    public void  setUp(){
-        driver = new ChromeDriver();
-    }
+public class LoginWiki extends TestBase {
+
 
     @Test
     public void openWikipediaTest() throws InterruptedException {
-        driver.get("https://www.wikipedia.org");
-        Thread.sleep(5000);
-
-        driver.findElement(By.id("js-link-box-en")).click();
-
-        Thread.sleep(5000);
-        driver.findElement(By.id("pt-login")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.id("wpName1")).click();
-        driver.findElement(By.id("wpName1")).clear();
-        driver.findElement(By.id("wpName1")).sendKeys("Eliyahu.Kosh");
-        Thread.sleep(5000);
-        driver.findElement(By.id("wpPassword1")).click();
-        driver.findElement(By.id("wpPassword1")).clear();
-        driver.findElement(By.id("wpPassword1")).sendKeys("wiki23081967");
-        Thread.sleep(5000);
-        driver.findElement(By.id("wpLoginAttempt")).click();
-        Thread.sleep(5000);
+        openSite("https://www.wikipedia.org");
+        pause();
+        selectLangEN();
+        pause();
+        initlogin();
+        pause();
+        //fill login form
+        fillLoginForm("Eliyahu.Kosh", "wiki23081967");
+        pause();
+        clickLogInButton();
 
     }
 
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
-
-    }
 
 }
